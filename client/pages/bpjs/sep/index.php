e21cb97da74f3ee2a883123313c5c3ad50f3f5a5
pondokcoder2020 <pondokcoder2020@github.com>
2021-04-07 14:52:49 +0700

    Kategori

--- client/pages/bpjs/sep/index.php
+++ client/pages/bpjs/sep/index.php
@@ -38,18 +38,20 @@
                                     </div>
                                 </div>
                             </div>
+                            <div class="card card-body">
+                                <table class="table table-bordered table-striped largeDataType" id="table-sep">
+                                    <thead class="thead-dark">
+                                    <tr>
+                                        <th class="wrap_content">No</th>
+                                        <th style="width: 10%;">SEP</th>
+                                        <th>Pasien</th>
+                                        <th class="wrap_content">Aksi</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody></tbody>
+                                </table>
+                            </div>
                         </div>
-                        <table class="table table-bordered table-striped largeDataType" id="table-sep">
-                            <thead class="thead-dark">
-                            <tr>
-                                <th class="wrap_content">No</th>
-                                <th style="width: 10%;">SEP</th>
-                                <th>Pasien</th>
-                                <th class="wrap_content">Aksi</th>
-                            </tr>
-                            </thead>
-                            <tbody></tbody>
-                        </table>
                     </div>
                 </div>
             </div>
